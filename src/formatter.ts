90c980b5fa5108992d54c34aa7d49dcaeba97823
Sega <brajicsergej@gmail.com>
2025-06-28 19:04:17 +0200

    Small improvements

--- src/formatter.ts
+++ src/formatter.ts
@@ -1,21 +1,22 @@
-export function formatJSON<jsonType>(
+export const formatJSON = <jsonType extends Object>(
   value: jsonType,
   spaces: number | string
-): string | undefined {
+): string | undefined => {
   try {
-    if (typeof value != "object")
+    if (typeof value !== "object" || value === null)
       throw new Error("Passed value in formatJSON() is not an object");
     if (typeof spaces != "number" && typeof spaces != "string")
       throw new Error("Passed number of spaces can only be string or a number");
 
-    spaces = Number(spaces);
+    let parsedSpaces = Number(spaces);
 
-    if (Number.isNaN(spaces))
+    if (Number.isNaN(parsedSpaces))
       throw new Error("Number of spaces has to be a number");
 
-    const JSONFormatted = JSON.stringify(value, null, spaces);
+    const JSONFormatted = JSON.stringify(value, null, parsedSpaces);
     return JSONFormatted;
   } catch (error) {
     console.error(error);
+    return undefined;
   }
-}
+};
