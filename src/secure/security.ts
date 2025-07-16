6b2ba87915712bd736eaaed03eb73022e6a15dd6
Sega <brajicsergej@gmail.com>
2025-07-16 18:56:56 +0200

    New wrapper functions for encrypting  and decrypting

--- src/secure/security.ts
+++ src/secure/security.ts
@@ -1,7 +1,14 @@
+import { decryptText, encryptText } from "../@helpers/cryptoutils";
 import {
   prohibitedKeys,
   prohibitedKeysBasic,
 } from "../@helpers/prohibitedKeys";
+
+interface EncryptParams {
+  encryptedData: string;
+  iv: string;
+  key: string;
+}
 
 export const stripValues = (
   object: Record<string, any | undefined>
@@ -14,13 +21,25 @@ export const stripValues = (
     return undefined;
   }
 };
-export const encrypt = () => {};
+
+export const encrypt = async (plain: string): Promise<Object> => {
+  return await encryptText(plain);
+};
+
 export const encryptAll = () => {};
-export const decrypt = () => {};
+
+export const decrypt = async ({
+  encryptedData,
+  iv,
+  key,
+}: EncryptParams): Promise<string> => {
+  return await decryptText(encryptedData, iv, key);
+};
 
 export const getPK = (): Set<string> => {
   return prohibitedKeys;
 };
+
 export const getPKBasic = (): Set<string> => {
   return prohibitedKeysBasic;
 };
